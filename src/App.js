202799df7c3ca6cc7e2a126b3c0445b7a2de44a2
Anthony Powell <adpowell@mtu.edu>
2019-08-26 07:37:52 -0400

    Install react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  return <div />;
+  return (
+    <Router>
+      <Route path="/" exact component={() => <p>Landing</p>} />
+      <Route path="/home" component={() => <p>Home</p>} />
+    </Router>
+  );
 }
 
 export default App;
