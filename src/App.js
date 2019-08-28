8c129a99c3b4cfa873bc2f7b6eed5493fdd67d61
Anthony Powell <adpowell@mtu.edu>
2019-08-28 16:50:59 -0400

    Setup theme provider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import ThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createTheme from "@material-ui/core/styles/createMuiTheme";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 // Components
@@ -7,15 +9,17 @@ import NavBar from "./components/navbar";
 import Landing from "./pages/landing";
 import Home from "./pages/home";
 
+const theme = createTheme({});
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <NavBar />
       <Router>
         <Route path="/" exact component={Landing} />
         <Route path="/home" component={Home} />
       </Router>
-    </>
+    </ThemeProvider>
   );
 }
 
