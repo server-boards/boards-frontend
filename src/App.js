a25393cb43e6dfd0c78fe256afec7d881cc461a8
Anthony Powell <adpowell@mtu.edu>
2019-08-28 14:47:04 -0400

    Scaffold navbar component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+// Components
+import NavBar from "./components/navbar";
+// Pages
 import Landing from "./pages/landing";
 import Home from "./pages/home";
 
 function App() {
   return (
-    <Router>
-      <Route path="/" exact component={Landing} />
-      <Route path="/home" component={Home} />
-    </Router>
+    <>
+      <NavBar />
+      <Router>
+        <Route path="/" exact component={Landing} />
+        <Route path="/home" component={Home} />
+      </Router>
+    </>
   );
 }
 
